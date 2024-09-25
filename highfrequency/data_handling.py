e78832fae73658cb747161364df9f454d3f5ef37
patricklucescu <patricklucescu@outlook.com>
2024-09-25 21:00:33 +0200

    updated to use Enums for column types

--- highfrequency/data_handling.py
+++ highfrequency/data_handling.py
@@ -2,51 +2,38 @@ import polars as pl
 from internal_data_handling import *
 from typing import List
 from helpers.exchange_names import EXCHANGES
+from helpers.schemas import TColumns, QColumns
 
 
 def select_exchange(
-        df: pl.DataFrame,
-        exchange: str = "N",
-        exchange_column: str = 'ex',
-        time_column: str = 'dt') -> pl.DataFrame:
+    t_data: pl.DataFrame,
+    exchange: str = "N",
+) -> pl.DataFrame:
     """
-    | Filter the data to retain only the data from the specified exchange. Be default the NYSE is selected.
+    | Filter the Raw Trade data to retain only the data from the specified exchange. Be default the NYSE is selected.
 
-    :param df: DataFrame containing data.
+    :param t_data: DataFrame containing raw trade data.
     :param exchange: Name of the exchange to filter the data for.
-    :param exchange_column: Name of the column containing the exchange information.
-    :param time_column: Name of the column containing the trade time.
     :return:
     """
 
-    # Check if 'df' is a Polars DataFrame
-    if not isinstance(df, pl.DataFrame):
-        raise ValueError("Input has to be a polars DataFrame.")
-
-    # Ensure required column names are present
-    if exchange_column not in df.columns:
-        raise ValueError(f"Data needs '{exchange_column}' column.")
-
-    # Check for "dt" column if needed for further processing
-    if time_column not in df.columns:
-        raise ValueError(f"Data needs '{time_column}' column.")
+    # check data consistency
+    t_data = check_column_names(t_data)
+    t_data = check_trade_data(t_data)
 
     # ensure the exchange selected is one of the allowed exchanges
     if exchange not in EXCHANGES:
         raise ValueError(f"Exchange '{exchange}' not found in the list of exchanges")
 
     # Filter the data where 'ex' column matches the specified 'exchange' value
-    filtered_data = df.filter(pl.col(exchange_column) == exchange)
+    filtered_data = t_data.filter(pl.col(TColumns.EXCHANGE.value.name) == exchange)
 
-    return filtered_data.sort(time_column)
+    return filtered_data.sort(TColumns.TIME.value.name)
 
 
 def auto_select_exchange_trades(
-        t_data: pl.DataFrame,
-        volume_column: str = 'size',
-        exchange_column: str = 'ex',
-        time_column: str = 'dt',
-        print_exchange: bool = False) -> pl.DataFrame:
+    t_data: pl.DataFrame, print_exchange: bool = False
+) -> pl.DataFrame:
     """
     | Retain only data from the stock exchange with the highest trading volume.
 
@@ -56,38 +43,34 @@ def auto_select_exchange_trades(
     or a custom list.
 
     :param t_data: polars DataFrame containing trade data with at least columns *size* and *ex*.
-    :param volume_column: Name of the column containing the trade volume.
-    :param exchange_column: Name of the column containing the exchange.
-    :param time_column: Name of the column containing the trade time.
     :param print_exchange: If True, print the exchange with the highest trading volume.
     :return: DataFrame containing only data from the exchange with the highest trading volume.
     """
-
+    # check data consistency
     t_data = check_column_names(t_data)
     t_data = check_trade_data(t_data)
 
-    if exchange_column not in t_data.columns:
-        raise ValueError(f"Data needs '{exchange_column}' column.")
-
     # Step 1: Group by 'symbol' and 'exchange', and aggregate 'size' by summing
-    aggregated_df = t_data.group_by(['symbol', exchange_column]).agg(pl.sum(volume_column).alias('total_size'))
+    aggregated_df = t_data.group_by(
+        [TColumns.SYMBOL.value.name, TColumns.EXCHANGE.value.name]
+    ).agg(pl.sum(TColumns.EXCHANGE.value.name).alias("total_size"))
 
     # Step 2: Find the maximum size exchange for each symbol
-    max_size_df = aggregated_df.group_by('symbol').agg(
-        pl.max('total_size').alias('max_size')
+    max_size_df = aggregated_df.group_by(TColumns.SYMBOL.value.name).agg(
+        pl.max("total_size").alias("max_size")
     )
 
     # Step 3: Join the maximum size with the original aggregated dataframe to filter the exchanges
-    joined_df = aggregated_df.join(max_size_df, on='symbol', how='inner')
+    joined_df = aggregated_df.join(max_size_df, on="symbol", how="inner")
 
     # Step 4: Filter to keep only the exchanges with the maximum size for each symbol
-    filtered_df = joined_df.filter(pl.col('total_size') == pl.col('max_size'))
+    filtered_df = joined_df.filter(pl.col("total_size") == pl.col("max_size"))
 
     # Step 5: Join back to the original dataframe to get the original rows
     result_df = t_data.join(
-        filtered_df[['symbol', exchange_column]],
-        on=['symbol', exchange_column],
-        how='inner'
+        filtered_df[[TColumns.SYMBOL.value.name, TColumns.EXCHANGE.value.name]],
+        on=[TColumns.SYMBOL.value.name, TColumns.EXCHANGE.value.name],
+        how="inner",
     )
 
     # Print the exchange with the highest trading volume
@@ -96,45 +79,30 @@ def auto_select_exchange_trades(
             symbol, exchange, _, _ = row
             print(f"Symbol: {symbol}, Selected Exchange: {EXCHANGES[exchange]}")
 
-    return result_df.sort(time_column)
+    return result_df.sort(TColumns.TIME.value.name)
 
 
-
-
-
-def remove_zero_prices(
-        df: pl.DataFrame,
-        price_column: str = 'price',
-        time_column: str = 'dt') -> pl.DataFrame:
+def remove_zero_prices(t_data: pl.DataFrame) -> pl.DataFrame:
     """
     | Remove rows with zero prices from the DataFrame.
 
-    :param df: polars DataFrame containing trade data.
-    :param price_column: Name of the column containing the trade price.
-    :param time_column: Name of the column containing the trade time.
+    :param t_data: polars DataFrame containing raw trade data.
     :return: DataFrame with rows containing zero prices removed.
     """
 
     # check data consistency
-    df = check_column_names(df)
-    df = check_trade_data(df)
+    t_data = check_column_names(t_data)
+    t_data = check_trade_data(t_data)
 
     # return non zero price dataframes
-    return df.filter(df[price_column] != 0).sort(time_column)
+    return t_data.filter(t_data[TColumns.PRICE.value.name] != 0).sort(TColumns.TIME.value.name)
 
 
-def remove_zero_quotes(
-        df: pl.DataFrame,
-        bid_column: str = 'bid',
-        ofr_column: str = 'ofr',
-        time_column: str = 'dt') -> pl.DataFrame:
+def remove_zero_quotes(df: pl.DataFrame) -> pl.DataFrame:
     """
     | Remove rows with zero bid and offer prices from the DataFrame.
 
     :param df: polars DataFrame containing quote data.
-    :param bid_column: Name of the column containing the bid price.
-    :param ofr_column: Name of the column containing the offer price.
-    :param time_column: Name of the column containing the quote time.
     :return: DataFrame with rows containing zero quotes removed.
     """
 
@@ -143,24 +111,28 @@ def remove_zero_quotes(
     df = check_quote_data(df)
 
     # remove zero quotes and sort by time
-    return df.filter((df[bid_column] != 0) & (df[ofr_column] != 0)).sort(time_column)
+    return df.filter(
+        (df[QColumns.BID.value.name] != 0) & (df[QColumns.OFR.value.name] != 0)
+    ).sort(QColumns.TIME.value.name)
 
 
 def exchange_hours_only(
-        df: pl.DataFrame,
-        market_open: tuple[int] = (9, 30),
-        market_close: tuple[int] = (16, 0),
-        time_column: str = 'dt') -> pl.DataFrame:
+    t_data: pl.DataFrame,
+    market_open: tuple[int] = (9, 30),
+    market_close: tuple[int] = (16, 0),
+) -> pl.DataFrame:
     """
-    | Filter DataFrame to contain only data from regular trading hours.
+    | Filter Raw Trade DataFrame to contain only data from regular trading hours.
 
-    :param df: DataFrame containing trade data.
+    :param t_data: DataFrame containing raw trade data.
     :param market_open: Tuple containing the opening time of the market. First entry represents the hour, second the minute.
     :param market_close: Tuple containing the closing time of the market. First entry represents the hour, second the minute.
-    :param time_column: Name of the column containing the trade time.
     :return: DataFrame containing only data from regular trading hours.
     """
 
+    # check data consistency
+    t_data = check_column_names(t_data)
+    t_data = check_trade_data(t_data)
 
     # check market open and close are consistent
     if len(market_open) != 2 or len(market_close) != 2:
@@ -175,36 +147,33 @@ def exchange_hours_only(
     market_close_duration = pl.duration(hours=market_close[0], minutes=market_close[1])
 
     # filter the data
-    df_filtered = df.filter(
-        (pl.col(time_column) >= pl.col(time_column).dt.truncate('1d') + market_open_duration) &
-        (pl.col(time_column) <= pl.col(time_column).dt.truncate('1d') + market_close_duration)
+    df_filtered = t_data.filter(
+        (
+            pl.col(TColumns.TIME.value.name)
+            >= pl.col(TColumns.TIME.value.name).dt.truncate("1d") + market_open_duration
+        )
+        & (
+            pl.col(TColumns.TIME.value.name)
+            <= pl.col(TColumns.TIME.value.name).dt.truncate("1d") + market_close_duration
+        )
     )
-    return df_filtered.sort(time_column)
+    return df_filtered.sort(TColumns.TIME.value.name)
 
 
 def remove_negative_spread(
-        q_data: pl.DataFrame,
-        time_column: str = 'dt',
-        bid_column: str = 'bid',
-        ofr_column: str = 'ofr'
+    q_data: pl.DataFrame,
 ) -> pl.DataFrame:
     """
-    | Remove rows with negative spread from the DataFrame.
+    | Remove rows with negative spread from the raw Quote data.
 
     :param q_data: DataFrame containing quote data.
-    :param time_column: Name of the column containing the quote time.
-    :param bid_column: Name of the column containing the bid price.
-    :param ofr_column: Name of the column containing the offer price.
     :return: DataFrame containing only data with positive spread.
     """
 
-    # Check if q_data is a polars DataFrame
-    if not isinstance(q_data, pl.DataFrame):
-        raise ValueError("Input has to be a polars DataFrame.")
-
+    # check data consistency
     q_data = check_column_names(q_data)
     q_data = check_quote_data(q_data)
 
-    filtered_data = q_data.filter(pl.col(ofr_column) > pl.col(bid_column))
+    filtered_data = q_data.filter(pl.col(QColumns.OFR.value.name) > pl.col(QColumns.BID.value.name))
 
-    return filtered_data.sort(time_column)
+    return filtered_data.sort(QColumns.TIME.value.name)
--- highfrequency/internal_data_handling.py
+++ highfrequency/internal_data_handling.py
@@ -1,6 +1,6 @@
 import polars as pl
 import warnings
-from helpers.exchange_names import EXCHANGES
+from helpers.schemas import TColumns, QColumns, column_name_mapper
 
 
 def check_trade_data(df: pl.DataFrame) -> pl.DataFrame:
@@ -17,23 +17,19 @@ def check_trade_data(df: pl.DataFrame) -> pl.DataFrame:
 
     df_schema = df.schema
 
-    if 'price' not in df_schema:
-        raise KeyError(f"Column 'price' not found in DataFrame columns.")
+    for column in TColumns:
+        # check column is present
+        if column.value.name not in df_schema:
+            raise KeyError(f"Column '{column.value.name}' not found in DataFrame columns.")
 
-    if 'dt' not in df_schema:
-        raise KeyError(f"Column 'dt' not found in DataFrame columns.")
-
-    if 'symbol' not in df_schema and 'sym_root' not in df_schema:
-        warnings.warn(f"Column 'symbol' (or 'sym_root') not found in DataFrame columns.", UserWarning)
-
-    if df_schema['price'] != pl.Float64:
-        raise TypeError(f"Column 'price' should be of type Float64.")
-
-    if df_schema['size'] not in [pl.Float64, pl.Int64]:
-        raise TypeError(f"Column 'size' should be of type Float64 or Int64.")
-
-    if df_schema['dt'] != pl.Datetime:
-        raise TypeError(f"Column 'dt' should be of type Datetime.")
+        # check column data type
+        if column.value.dtype is not None:
+            if not isinstance(column.value.dtype, list):
+                if df_schema[column.value.name] != column.value.dtype:
+                    raise TypeError(f"Column '{column.value.name}' should be of type {column.value.dtype}.")
+            else:
+                if not any(df_schema[column.value.name] == dtype for dtype in column.value.dtype):
+                    raise TypeError(f"Column '{column.value.name}' should be of type {column.value.dtype}.")
 
     return df
 
@@ -52,33 +48,19 @@ def check_quote_data(df: pl.DataFrame) -> pl.DataFrame:
 
     df_schema = df.schema
 
-    if 'dt' not in df_schema:
-        raise KeyError(f"Column 'dt' not found in DataFrame columns.")
+    for column in QColumns:
+        # check column is present
+        if column.value.name not in df_schema:
+            raise KeyError(f"Column '{column.value.name}' not found in DataFrame columns.")
 
-    if 'bid' not in df_schema:
-        raise KeyError(f"Column 'bid' not found in DataFrame columns.")
-
-    if 'ofr' not in df_schema:
-        raise KeyError(f"Column 'ofr' not found in DataFrame columns.")
-
-    if 'symbol' not in df_schema and 'sym_root' not in df_schema:
-        warnings.warn(f"Column 'symbol' (or 'sym_root') not found in DataFrame columns.", UserWarning)
-
-    if df_schema['bid'] != pl.Float64:
-        raise TypeError(f"Column 'bid' should be of type Float64.")
-
-    if df_schema['ofr'] != pl.Float64:
-        raise TypeError(f"Column 'ofr' should be of type Float64.")
-
-    if df_schema['bidsiz'] not in [pl.Float64, pl.Int64]:
-        raise TypeError(f"Column 'bidsiz' should be of type Float64 or Int64.")
-
-    if df_schema['ofrsiz'] not in [pl.Float64, pl.Int64]:
-        raise TypeError(f"Column 'ofrsiz' should be of type Float64 or Int64.")
-
-    if df_schema['dt'] != pl.Datetime:
-        raise TypeError(f"Column 'dt' should be of type Datetime.")
-
+        # check column data type
+        if column.value.dtype is not None:
+            if not isinstance(column.value.dtype, list):
+                if df_schema[column.value.name] != column.value.dtype:
+                    raise TypeError(f"Column '{column.value.name}' should be of type {column.value.dtype}.")
+            else:
+                if not any(df_schema[column.value.name] == dtype for dtype in column.value.dtype):
+                    raise TypeError(f"Column '{column.value.name}' should be of type {column.value.dtype}.")
     return df
 
 
@@ -87,27 +69,14 @@ def check_column_names(df: pl.DataFrame) -> pl.DataFrame:
     | Sets column names according to RTAQ format using quantmod conventions, such that all the other functions find
     the correct information.
 
-    :param df: polars DataFrame containing trade data.
+    :param df: polars DataFrame containing TAQ data.
     :return: DataFrame with column names set according to RTAQ format.
     """
 
     # lowercase all column names
     df = df.rename({col: col.lower() for col in df.columns})
 
-    # Change column names to previous RTAQ format
-    rename_map = {
-        "ask": "ofr",
-        "sym_root": "symbol",
-        "bidsize": "bidsiz",
-        "asksize": "ofrsiz",
-        "asksiz": "ofrsiz",
-        "ofrsize": "ofrsiz",
-        "tr_scond": "cond",
-        "cr": "corr",
-        "tr_corr": "corr"
-    }
-
     # Apply the renaming map only if the column exists in the dataframe
-    df = df.rename({old: new for old, new in rename_map.items() if old in df.columns})
+    df = df.rename({old: new for old, new in column_name_mapper.items() if old in df.columns})
 
     return df
